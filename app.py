09403b9a3438b8b98331d28eaf8909d87ab5fa45
Spielberg Michel <asheen16@gmail.com>
2021-05-29 12:47:49 -0700

    Updated main in app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request,jsonify, render_template
+import os
 app = Flask(__name__, static_folder='../public/', static_url_path='/')
 
 
@@ -17,4 +18,9 @@ def resume_bank_app():
 @app.route('/text_app')
 def text_app():
     return "Under construction!"
+
+
+if __name__ =="__main__":
+    port = int(os.environ.get("PORT",8000))
+    app.run(host='0.0.0.0',port = port)
 
