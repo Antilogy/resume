e2217d7ed2f82f5cf294caae20f6943f2f23481f
Spielberg Michel <asheen16@gmail.com>
2022-01-17 21:55:20 -0800

    Updated app to application

--- app.py
+++ app.py
@@ -1,34 +1,34 @@
 from flask import Flask, request,jsonify, render_template
 import os
-app = Flask(__name__,static_folder='dist', static_url_path='/')
+application = Flask(__name__,static_folder='dist', static_url_path='/')
 
 
-@app.route('/')
+@application.route('/')
 def resume_home():
-    return app.send_static_file("index.html")
+    return application.send_static_file("index.html")
 
 
 
-@app.route('/vue')
+@application.route('/vue')
 def resume_vue_home():
-    return app.send_static_file("vue_index.html")
+    return application.send_static_file("vue_index.html")
 
 
-@app.route('/bank-app')
+@application.route('/bank-app')
 def resume_bank_app():
     return "Under construction!"
 
-@app.route('/split_check')
+@application.route('/split_check')
 def split_check():
     return app.send_static_file("split_check.html")
 
 
-@app.route('/text_app')
+@application.route('/text_app')
 def text_app():
     return "Under construction!"
 
 
 if __name__ =="__main__":
     # port = int(os.environ.get("PORT",8000))
-    app.run(port = 5000, debug = True)
+    application.run(port = 5000, debug = True)
 
