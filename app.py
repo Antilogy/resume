13cd90fec516f20d04f3039b0d20ddf436fdee44
Spielberg Michel <asheen16@gmail.com>
2022-01-17 20:24:26 -0800

    Added .ebignore file.

--- app.py
+++ app.py
@@ -29,6 +29,6 @@ def text_app():
 
 
 if __name__ =="__main__":
-    port = int(os.environ.get("PORT",8000))
-    app.run(host='0.0.0.0',port = port)
+    # port = int(os.environ.get("PORT",8000))
+    app.run(port = 5000, debug = True)
 
